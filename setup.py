c3a85108d95f338e207a5d59d43212430d08f20c
Muhammad Hussnain <muhammadhussnain1227@gmail.com>
2026-03-13 06:35:51 +0500

    Updated version

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
